57f9c4042c1398779375c702ba143243f6d0502d
Wendell Cardoso <cardosowendell@gmail.com>
2021-04-15 23:28:32 -0300

    buscando dados da api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import Header from "./components/Header";
 import Form from "./components/Form";
 
 import "./styles/global.css";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [charactersRender, setCharactersRender] = useState([]);
+
+  useEffect(() => {
+
+    const publicKey = "683181e8d97212fef95e84879055cd63";
+
+    const hash = "371b267365d4480f7313c97c50a27a67";
+
+    const urlBase = "https://gateway.marvel.com/v1/public/characters";
+
+    const uri = `${urlBase}?ts=1&apikey=${publicKey}&hash=${hash}`;
+
+    const fetchCharacters = async () => {
+      // setLoading(true);
+      // const res = await axios.get(uri);
+      // setCharacters(res.data.data.results);
+      // setLoading(false);
+
+      // if(localStorage.getItem("dataTeste") == null){
+      //   localStorage.setItem("dataTeste", JSON.stringify(res.data.data.results));
+      // }
+    };
+    
+    // fetchData();
+
+    if(characters.length == 0){
+      setCharacters(JSON.parse(localStorage.getItem("dataTeste")));
+      setCharactersRender(JSON.parse(localStorage.getItem("dataTeste")));
+    };
+  }, []);
+
+  useEffect(() => {
+      console.log(charactersRender);
+  }, [charactersRender]);
+  
+  // console.log(characters);
+
+  //Alimentar o array de rederizacao
+
+  const manageResultsArray = (inputText) => {
+
+    if(inputText == ""){
+      setCharactersRender(characters);
+    }else{
+      setCharactersRender(characters.filter(character => 
+        character.name
+          .toLowerCase()
+          .includes(inputText.toLowerCase()))
+      ) 
+    }
+  }
   return (
     <div>
       <Header/>
       <div className="content">
         <h1>Busca de personagens</h1>
-        <Form />
+        <Form manageResultsArray={manageResultsArray} />
       </div>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/components/Form.module.css";
 
-const Form = () => {
+const Form = ({manageResultsArray}) => {
+
+    const [inputText, setInputText] = useState("");
+
+    const handleInputChange = (event) => {
+
+        const {value} = event.target;
+
+        setInputText(value);
+
+        manageResultsArray(value);
+
+    }
+
     return (
         <div className={styles.container}>
             <form>
                 <h2>Nome do personagem</h2>
                 <div className={styles.search}>
-                    <input type="text" placeholder="Search" />
+                    <input 
+                        type="text" 
+                        placeholder="Search"
+                        value={inputText}
+                        onChange={handleInputChange}
+                    />
                     <button type="submit">
                         <img src="assets/search.svg" />
                     </button>
