a9e3830766738941cd3cea3cb1a72bad25722f6e
Wendell Cardoso <cardosowendell@gmail.com>
2021-04-17 13:12:53 -0300

    responsividade do modal

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -7,13 +7,15 @@ import styles from "../styles/components/ModalContent.module.css";
 const ModalContent = ({closeModal}) => {
     return (
         <div>
-            <div className={styles.closeModal} onClick={closeModal}>
-                <img src="assets/close.svg" alt="Close Modal" />
+            <div className={styles.closeModal} >
+                <img src="assets/close.svg" alt="Close Modal" onClick={closeModal} />
             </div>
             <section className={styles.charDetails}>
                 <img src="teste/char.png"  />
-                <h2>Rick Jones</h2>
-                <p>"Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction! "</p>
+                <div>
+                    <h2>Rick Jones</h2>
+                    <p>"Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction! "</p>
+                </div>
             </section>
             <SeriesEvents titleSection="Séries" />
             <SeriesEvents titleSection="Eventos" />
